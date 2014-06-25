f2901612b6a435ea6faa6ca638d68d6e5215ff11
Johnathon Beauvais <jay_beauvais@live.com>
2014-06-25 00:09:55 -0400

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 $TESTING=true
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
+
+
+       underscored_to_human_readable( word.underscore )
+      end
+      
+ +    def non_id_columns(model)
+ +      model.columns.select{ |col| col.to_s !~ /_id$/ }
+ +    end
+ +    
+      def column_type(model, column)
+        model.schema.create_info.first.find{ |c| c[:name] == column.to_sym }[:type]
+      end
